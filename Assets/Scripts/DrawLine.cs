9ce7b5538f6d4051ba3ef831896c258aff76f75b
Xiuxuan <xiuxuanl@usc.edu>
2021-06-01 22:53:37 -0700

    Create a script drawing the line
    Follow the youtube video to create draw lines but the player can't collide with the line.

---
+++ Assets/Scripts/DrawLine.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawLine : MonoBehaviour
+{
+    public GameObject linePrefab;
+    // hold our current line object
+    public GameObject currentLine;
+
+    public LineRenderer lineRenderer;
+    public EdgeCollider2D edgeCollider;
+    
+    // receive input from the user
+    public List<Vector2> fingerPositions;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // left click
+        if (Input.GetMouseButtonDown(0))
+        {
+            CreateLine();
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            Vector2 tempFingerPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            if (Vector2.Distance(tempFingerPos, fingerPositions[fingerPositions.Count - 1]) > .1f)
+            {
+                UpdateLine(tempFingerPos);
+            }
+        }
+    }
+
+    void CreateLine()
+    {
+        currentLine = Instantiate(linePrefab, Vector3.zero, Quaternion.identity);
+        lineRenderer = currentLine.GetComponent<LineRenderer>();
+        edgeCollider = currentLine.GetComponent<EdgeCollider2D>();
+        // Create a new line
+        fingerPositions.Clear();
+        // The line needs at least two points to be drown
+        fingerPositions.Add(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+        fingerPositions.Add(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+        lineRenderer.SetPosition(0, fingerPositions[0]);
+        lineRenderer.SetPosition(1, fingerPositions[1]);
+        
+        // update the edge collider
+        edgeCollider.points = fingerPositions.ToArray();
+    }
+
+    void UpdateLine(Vector2 newFingerPos)
+    {
+        fingerPositions.Add(newFingerPos);
+        lineRenderer.positionCount++;
+        lineRenderer.SetPosition(lineRenderer.positionCount-1, newFingerPos);
+        
+    }
+}
