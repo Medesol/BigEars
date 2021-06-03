b5b7449c0bda2cd54fa4aa6662feb9025f5b2325
Xiuxuan <xiuxuanl@usc.edu>
2021-06-03 07:34:59 -0700

    get edgeCollider2d in the lineController

--- Assets/Scripts/LineController.cs
+++ Assets/Scripts/LineController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,16 +7,22 @@ public class LineController : MonoBehaviour
 {
 
     public Rigidbody2D rigidBody;
+
+    public EdgeCollider2D edgeCollider;
     // Start is called before the first frame update
     void Start()
     {
         rigidBody = gameObject.GetComponent<Rigidbody2D>();
+        edgeCollider = gameObject.GetComponent<EdgeCollider2D>();
         //rigidBody.centerOfMass = new Vector2(-100, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (rigidBody.bodyType == RigidbodyType2D.Dynamic)
+        {
+            rigidBody.centerOfMass = rigidBody.position;
+        }
     }
 }
