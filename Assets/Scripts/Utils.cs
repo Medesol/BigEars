fb8a08ad4afb79c57fded4ae73567cd45158a666
JingDong <874753722@qq.com>
2021-06-08 17:47:16 -0700

    erasing drawn lines

---
+++ Assets/Scripts/Utils.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public static class Utils
+{
+    public static GameObject Raycast(Camera mainCamera, Vector2 screenPosition, int layermask) {
+    	Ray ray = mainCamera.ScreenPointToRay(screenPosition);
+    	RaycastHit2D hit2D = Physics2D.GetRayIntersection(ray, Mathf.Infinity, layermask);
+    	if (hit2D.collider != null) return hit2D.collider.gameObject;
+    	return null;
+    }
+}
