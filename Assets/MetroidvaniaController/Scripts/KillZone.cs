c3259f76105c508979254bd5c7ea2a832898985c
guancongyi <congyiguan@gmail.com>
2021-06-23 15:32:29 +0800

    added analytics for player fall down events

--- Assets/MetroidvaniaController/Scripts/KillZone.cs
+++ Assets/MetroidvaniaController/Scripts/KillZone.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Analytics;
 using UnityEngine.SceneManagement;
 
 public class KillZone : MonoBehaviour
@@ -9,6 +10,18 @@ public class KillZone : MonoBehaviour
     {
         if (col.gameObject.tag == "Player")
         {
+            AnalyticsResult gameResult = Analytics.CustomEvent(
+                "gameOver_fall_below",
+                new Dictionary<string, object>
+                {
+                    { "level name", SceneManager.GetActiveScene().name },
+                    { "progress", (col.gameObject.transform.position.x + 5.5)/70 }
+                }
+            );
+
+            //Debug.Log("scecne: " + SceneManager.GetActiveScene().name);
+            //Debug.Log("Log: " + (col.gameObject.transform.position.x + 5.5)/70);
+            Debug.Log("status: " + gameResult);
             SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex);
         }
         else
