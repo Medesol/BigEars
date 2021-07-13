2d35452ed4dfbc9cd9b952abd77fa57ac554241d
XinnXuu <xxu79561@usc.edu>
2021-07-13 22:05:35 +0800

    Add simple start menu and level selection menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void NewGame()
+    {
+        SceneManager.LoadScene("Level0-Scene");
+        Debug.Log("Game Start");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void SelectLevel(int i)
+    {
+        SceneManager.LoadSceneAsync(i);
+    }
+    
+}
